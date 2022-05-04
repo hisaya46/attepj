cb7f6c07e7f9b238a791c2a35723fc81e7f4a966
hisaya46 <46.hisaya@gmail.com>
2022-05-04 17:09:22 +0900

    [500エラー]を解消

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -23,7 +23,7 @@ class AttendanceController extends Controller
         $today = Carbon::today()->format('Y-m-d');
         $now = Carbon::now()->format('Y-m-d'); //dateの比較に使用
         $attendance = Attendance::where('user_id', $user->id)->where('date', $today)->first();
-        $past = Attendance::latest()->where('user_id', $user->id)->where('date','<', $today)->first(); //過去のdateで最新のものを取得
+        $past = Attendance::where('user_id', $user->id)->where('date','<', $today)->latest()->first(); //過去のdateで最新のものを取得
         $straddle = Attendance::where('user_id', $user->id)->latest()->first(); //日跨ぎ時の時刻の更新に使用
 
         // 出勤したまま日を跨いだ場合、end_timeを'23:59:59'に更新
