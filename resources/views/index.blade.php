1b0ac6a3e6f0bcd3ce6c062be36b1382028b7c79
hisaya46 <46.hisaya@gmail.com>
2022-05-10 12:29:03 +0900

    message

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,32 +8,28 @@
 <div class="attendance">
     <h2 class="register-field-ttl">{{(Auth::user())->name}}さん、お疲れ様です！</h2>
     <div class="attendance-field">
-        @if(isset($btn))
-        @else
-        <p>メッセージは存在しません。</p>
-        @endif
         <form class="attendance-form" action="{{ route('timestamp/start') }}" method="POST">
             @csrf
             <div class="attendance-form-items">
-                <input class="attendance-items-btn" id="workstart" type="submit" value="勤務開始" <?php if ($btn['workStart'] == false) { ?> disabled <?php } ?>>
+                <input class="attendance-items-btn" id="workstart" type="submit" value="勤務開始" <?php if ($btn['workStart'] = false) { ?> disabled <?php }else{ ?>>
             </div>
         </form>
         <form class="attendance-form" action="{{ route('timestamp/end') }}" method="POST">
             @csrf
             <div class="attendance-form-items">
-                <input class="attendance-items-btn" id="workend" type="submit" value="勤務終了" <?php if ($btn['workEnd'] == false) { ?> disabled <?php } ?>>
+                <input class="attendance-items-btn" id="workend" type="submit" value="勤務終了" <?php if ($btn['workEnd'] = false) { ?> disabled <?php } ?>>
             </div>
         </form>
         <form class="attendance-form" action="{{ route('timestamp/breakin') }}" method="POST">
             @csrf
             <div class="attendance-form-items">
-                <input class="attendance-items-btn" id="breakin" type="submit" value="休憩開始" <?php if ($btn['breakIn'] == false) { ?> disabled <?php } ?>>
+                <input class="attendance-items-btn" id="breakin" type="submit" value="休憩開始" <?php if ($btn['breakIn'] = false) { ?> disabled <?php } ?>>
             </div>
         </form>
         <form class="attendance-form" action="{{ route('timestamp/breakout') }}" method="POST">
             @csrf
             <div class="attendance-form-items attendance-form-items-last ">
-                <input class="attendance-items-btn" id="breakout" type="submit" value="休憩終了" <?php if ($btn['breakOut'] == false) { ?> disabled <?php } ?>>
+                <input class="attendance-items-btn" id="breakout" type="submit" value="休憩終了" <?php if ($btn['breakOut'] = false) { ?> disabled <?php } ?>>
             </div>
         </form>
     </div>
